2179c875bc3ed1e31b74495acfae5384457f3ad9
Martin Del Rio <martindelrio@Martins-MacBook-Pro.local>
2023-10-29 20:40:12 +0800

    Dot Connect

--- Item/Dot_lock/dot_lock.gd
+++ Item/Dot_lock/dot_lock.gd
@@ -5,12 +5,13 @@ var area_i : Area2D
 
 func _process(delta):
 	if area_i:
-		pass
+		area_i.global_position = global_position
 	
 
 func _on_dot_lock_area_entered(area):
-	print("lock")
+	if area_i:
+		return
 	if area.is_in_group('dot_lock'):
 		print("connect")
-	locked = true
-	area_i = area
+		locked = true
+		area_i = area
